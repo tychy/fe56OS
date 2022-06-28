ad6f36dc17e1dc2d898b34f56f56ea71b158a161
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-06-29 00:11:24 +0900

    placement new

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -18,6 +18,9 @@ void operator delete(void *obj) noexcept
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console *console;
+
 extern "C" void
 KernelMain(const struct FrameBufferConfig &frame_buffer_config)
 {
@@ -40,13 +43,13 @@ KernelMain(const struct FrameBufferConfig &frame_buffer_config)
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
   for (int i = 0; i < 25; i++)
   {
     char buf[128];
     sprintf(buf, "line %d\n", i);
 
-    console.PutString(buf);
+    console->PutString(buf);
   }
 
   while (1)
