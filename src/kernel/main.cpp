c876787cb51ebb6871458e1e99151a2ce6f1642a
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-12-09 17:47:47 +0900

    wip

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -8,6 +8,8 @@
 #include "font.hpp"
 #include "console.hpp"
 #include "pci.hpp"
+#include "interrupt.hpp"
+#include "asmfunc.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "usb/classdriver/mouse.hpp"
 
@@ -64,6 +66,21 @@ void SwitchEhci2Xhc(const pci::Device &xhc_dev)
   printk("SwitchEhci2Xhci: SS = %02x, xHCI = %02x\n", supersped_ports, ehci2xhci_ports);
 }
 
+usb::xhci::Controller *xhc;
+
+__attribute__((interrupt)) void IntHandlerXHCI(InterruptFrame *frame)
+{
+  while (xhc->PrimaryEventRing()->HasFront())
+  {
+    if (auto err = ProcessEvent(*xhc))
+    {
+      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+          err.Name(), err.File(), err.Line());
+    }
+  }
+  NotifyEndOfInterrupt();
+}
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
@@ -111,7 +128,7 @@ KernelMain(const struct FrameBufferConfig &frame_buffer_config)
   console = new (console_buf) Console{
       *pixel_writer, kDesktopFGColor, kDesktopBGColor};
   printk("Welcome to MikanOS!\n");
-  SetLogLevel(kWarn);
+  SetLogLevel(kDebug);
 
   mouse_cursor = new (mouse_cursor_buf) MouseCursor{
       pixel_writer, kDesktopBGColor, {300, 200}};
@@ -144,7 +161,18 @@ KernelMain(const struct FrameBufferConfig &frame_buffer_config)
     Log(kDebug, "xHC has been found: %d.%d.%d", xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
 
-  const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+  const uint16_t cs = GetCS();
+  SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
+  LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
+
+  const uint8_t bsp_local_apic_id = *reinterpret_cast<const uint32_t *>(0xfee00020) >> 24;
+  pci::ConfigureMSIFixedDestination(
+      *xhc_dev, bsp_local_apic_id,
+      pci::MSITriggerMode::kLevel, pci::MSIDeliveryMode::kFixed,
+      InterruptVector::kXHCI, 0);
+
+  const WithError<uint64_t>
+      xhc_bar = pci::ReadBar(*xhc_dev, 0);
   Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
   const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
   Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
@@ -177,15 +205,6 @@ KernelMain(const struct FrameBufferConfig &frame_buffer_config)
             err.Name(), err.File(), err.Line());
         continue;
       }
-    }
-  }
-
-  while (1)
-  {
-    if (auto err = ProcessEvent(xhc))
-    {
-      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
-          err.Name(), err.File(), err.Line());
     }
   }
 
